aaf671f30c5682100e87f815f1f842f29b4ebc75
Vinicius Miranda <vini.s.miranda@hotmail.com>
2022-11-09 22:35:24 -0300

    [ADD] card repository: adding card repository

---
+++ backend/src/main/java/com/forbiddenkey/repositories/CardRepository.java
@@ -0,0 +1,9 @@
+package com.forbiddenkey.repositories;
+
+import com.forbiddenkey.entities.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long>{
+}
