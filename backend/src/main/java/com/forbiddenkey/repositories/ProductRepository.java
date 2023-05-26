3a146179e64483c55a232056dd992afb0a720ebd
vinicius <vini.s.miranda@hotmail.com>
2023-05-26 00:22:54 -0300

    [ADD] get all available

--- backend/src/main/java/com/forbiddenkey/repositories/ProductRepository.java
+++ backend/src/main/java/com/forbiddenkey/repositories/ProductRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+    @Query(value = "SELECT * FROM TB_PRODUCT WHERE ACTIVE = TRUE AND QUANTITY != 0", nativeQuery = true)
     List<Product> findByActiveTrue();
 
     @Query(value = "SELECT TOP 5 * FROM TB_PRODUCT WHERE ACTIVE = TRUE AND SELLED_QUANTITY != 0 ORDER BY SELLED_QUANTITY DESC", nativeQuery = true)
